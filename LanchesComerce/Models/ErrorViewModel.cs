09b753ed86478075a02a302776ed5b0eacdf6d4f
92021 <92021@WEB-053>
2022-11-18 16:34:56 -0300

    Add project files.

---
+++ LanchesComerce/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LanchesComerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
